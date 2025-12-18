616a9a6c156b39ed7df3fa4fc3a3f443076a265c
Maximilian Kresse <545671+MaximilianKresse@users.noreply.github.com>
2025-12-18 18:42:41 +0100

    Reworked asset handling

--- src/components/MarketBuyItem.vue
+++ src/components/MarketBuyItem.vue
@@ -1,24 +1,46 @@
 <template>
   <img
     v-if="currentMarketState >= itemValue"
-    :src="'assets/' + marketType + '-token.png'"
+    :src="imageUrl"
     width="40"
     height="40"
     draggable="false"
     @click="newMarketState = itemValue"
     :class="{ isSelected: newMarketState !== null && newMarketState <= itemValue }"
   />
-  <img v-else src="assets/missing-token.png" width="40" height="40" draggable="false" />
+  <img v-else :src="imageUrl" width="40" height="40" draggable="false" />
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+import missingToken from '../assets/missing-token.png?inline';
+import foodToken from '../assets/food-token.png?inline';
+import syncaniteToken from '../assets/syncanite-token.png?inline';
+import goodsToken from '../assets/goods-token.png?inline';
+
+const props = defineProps<{
   itemValue: number;
   marketType: 'food' | 'syncanite' | 'goods';
   currentMarketState: number;
 }>();
 
 const newMarketState = defineModel<null | number>({ required: true });
+
+const imageUrl = computed(() => {
+  if (props.currentMarketState < props.itemValue) {
+    return missingToken;
+  }
+  if (props.marketType === 'food') {
+    return foodToken;
+  }
+  if (props.marketType === 'syncanite') {
+    return syncaniteToken;
+  }
+  if (props.marketType === 'goods') {
+    return goodsToken;
+  }
+  throw new Error('Unknown market type');
+});
 </script>
 
 <style scoped>
--- src/components/MarketStateItem.vue
+++ src/components/MarketStateItem.vue
@@ -1,22 +1,42 @@
 <template>
-  <img
-    :src="
-      currentMarketValue >= itemValue
-        ? 'assets/' + marketType + '-token.png'
-        : 'assets/missing-token.png'
-    "
-    width="40"
-    height="40"
-    draggable="false"
-    @click="currentMarketValue = itemValue"
-  />
+  <img :src="imageUrl" width="40" height="40" draggable="false" @click="onClick" />
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+import missingToken from '../assets/missing-token.png?inline';
+import foodToken from '../assets/food-token.png?inline';
+import syncaniteToken from '../assets/syncanite-token.png?inline';
+import goodsToken from '../assets/goods-token.png?inline';
+
+const props = defineProps<{
   itemValue: number;
   marketType: 'food' | 'syncanite' | 'goods';
 }>();
 
 const currentMarketValue = defineModel<number>({ required: true });
+
+const imageUrl = computed(() => {
+  if (currentMarketValue.value < props.itemValue) {
+    return missingToken;
+  }
+  if (props.marketType === 'food') {
+    return foodToken;
+  }
+  if (props.marketType === 'syncanite') {
+    return syncaniteToken;
+  }
+  if (props.marketType === 'goods') {
+    return goodsToken;
+  }
+  throw new Error('Unknown market type');
+});
+
+function onClick() {
+  if (props.itemValue === 1 && currentMarketValue.value === 1) {
+    currentMarketValue.value = 0;
+    return;
+  }
+  currentMarketValue.value = props.itemValue;
+}
 </script>
